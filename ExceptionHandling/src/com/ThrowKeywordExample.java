ccad0395125d56b2c0cd7ebc365b1f19b232d453
likitha <likithalikki6060@gmail.com>
2022-02-28 22:13:45 +0530

    initial commit

--- ExceptionHandling/src/com/ThrowKeywordExample.java
+++ ExceptionHandling/src/com/ThrowKeywordExample.java
@@ -1,24 +1,29 @@
 package com;
-
-public class ThrowsExample {
-
-	static void dis1() throws Exception{
-	//	try {
-		int a=10/0;
-	//	}catch(Exception e) {}
-		System.out.println("This is dis1 method");
+class MyException extends Exception {
+	public MyException() {
+		
 	}
-	static void dis2() throws Exception{
-		//try {
-		dis1();
-		//}catch(Exception e) {}
-		System.out.println("This is dis2 method");
+	public MyException(String msg) {
+			super(msg);			// calling super class ie Exception class parameterized constructor to set the message
 	}
-	public static void main(String[] args) throws Exception{
-			///try {
-			dis2();
-			//}catch(Exception e) {}
-			System.out.println("This is main method");
+}
+public class ThrowKeywordExample {
+
+	public static void main(String[] args) {
+	int a=10;
+	 int b=5;
+	 try {
+		 	if(a>b) {
+		 		//throw new Exception(); // pre-defined super class exception generate
+		 		//throw new Exception("a>b");	// pre-defined super class exception with customer message. 
+		 		//throw new ArithmeticException();	// pre-defined exception generated with custom condition
+		 		//throw new ArithmeticException("a>b");	
+		 		//throw new MyException();		// custom exception generated with custom condition 
+		 		throw new MyException("a>b");	// custom exception with custom message. 
+		 	}
+	 }catch (Exception e) {
+		System.out.println(e);
+	}
 	}
 
 }
